070d50936c34db3bd93cc8f4a2549d8db0fe10c2
Shushant Kumar <109276429+Shushant8051@users.noreply.github.com>
2022-08-05 16:14:54 +0530

    1st question code

---
+++ hackethon.js
@@ -0,0 +1,11 @@
+var maxmin=[];
+var size = 5;
+for (var a=0; a<size; a++)
+{
+    maxmin[a] = prompt('enter array element' + (a+1));
+
+}
+
+const maxmin = [];
+console.log(math.max(...maxmin));
+console.log(math.min(...maxmin));
